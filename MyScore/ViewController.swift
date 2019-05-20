c978b87b9a55c85c0ef93da4c8a41b55fa4ce762
Samuel Lavasani <samuel.lavasani@adlibris.com>
2019-05-20 10:31:50 +0200

    Initial Commit

---
+++ MyScore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyScore
+//
+//  Created by Samuel on 2019-05-20.
+//  Copyright © 2019 Samuel Lavasani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
