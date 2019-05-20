2902ed430d042d0a334369aafa5c723c4dac4639
Samuel Lavasani <samuel.lavasani@adlibris.com>
2019-05-20 11:08:15 +0200

    Pod install Alamofire

--- MyScore/ViewController.swift
+++ MyScore/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let FOOTBALL_URL = "http://api.football-data.org/v2/teams/18"
+    let APP_ID = "&appid=e394b0d42c6d3c58007da86d5627ba90"
+    let URL_LAT = "lat="
+    let URL_LONG = "&lon="
+    let URL_Q = "q="
 
     override func viewDidLoad() {
         super.viewDidLoad()
